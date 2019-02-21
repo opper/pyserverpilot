62c2304720e29e386c536d71fdf4c1dfd7789e40
Alex <alexraileanu@me.com>
2019-02-21 14:40:35 +0100

    [update_server] - refactor update server method; raise proper errors on invalid types;

--- pyserverpilot/modules/servers.py
+++ pyserverpilot/modules/servers.py
@@ -5,27 +5,31 @@ from pyserverpilot.models.server import Server
 
 ServerList = List[Server]
 
+SERVERS_BASE_ENDPOINT = 'servers'
+
 
 class Servers(Serverpilot):
     def get_servers(self) -> ServerList:
         servers = []
 
-        for server in self._request('GET', 'servers'):
+        for server in self._request('GET', SERVERS_BASE_ENDPOINT):
             servers.append(Server(server))
 
         return servers
 
     def get_server(self, id: str) -> Server:
-        return Server(self._request('GET', 'servers/{}'.format(id)))
+        return Server(self._request('GET', '{}/{}'.format(SERVERS_BASE_ENDPOINT, id)))
 
     def delete_server(self, id: str) -> None:
-        self._request('DELETE', 'servers{}'.format(id))
+        self._request('DELETE', '{}/{}'.format(SERVERS_BASE_ENDPOINT, id))
 
     def update_server(self, id: str, **params) -> Server:
         for key, val in params.items():
-            if key is 'plan' and type(val) is not str:
-                raise ValueError('plan argument has to be of type string')
-            if (key in ['firewall', 'autoupdates', 'deny_unknown_domains']) and val is not bool:
-                raise ValueError('{} argument has to be of type boolean'.format(key))
+            if key == 'plan' and isinstance(val, str) is False:
+                raise TypeError('plan argument has to be of type string')
+            elif key in ['firewall', 'autoupdates', 'deny_unknown_domains'] and isinstance(val, bool) is False:
+                raise TypeError('{} argument has to be of type boolean'.format(key))
+            else:
+                raise AttributeError('Found unexpected argument: {}'.format(key))
 
-        return Server(self._request('POST', 'servers/{}'.format(id), params))
+        return Server(self._request('POST', '{}/{}'.format(SERVERS_BASE_ENDPOINT, id), params))
