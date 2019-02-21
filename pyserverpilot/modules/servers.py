7314a579d88b77403bbd18bf07c9d275626f130d
Alex <alexraileanu@me.com>
2019-02-21 10:50:25 +0100

    [update_server] - added update server method

--- pyserverpilot/modules/servers.py
+++ pyserverpilot/modules/servers.py
@@ -17,3 +17,15 @@ class Servers(Serverpilot):
 
     def get_server(self, id: str) -> Server:
         return Server(self._request('GET', 'servers/{}'.format(id)))
+
+    def delete_server(self, id: str) -> None:
+        self._request('DELETE', 'servers{}'.format(id))
+
+    def update_server(self, id: str, **params) -> Server:
+        for key, val in params.items():
+            if key is 'plan' and type(val) is not str:
+                raise ValueError('plan argument has to be of type string')
+            if (key is 'firewall' or key is 'autoupdates' or key is 'deny_unknown_domains') and val is not bool:
+                raise ValueError('{} argument has to be of type boolean'.format(key))
+
+        return Server(self._request('POST', 'servers/{}'.format(id), params))
