df1d0aea59cb105da0997548c854ecc418f0a6cf
Alex <alexraileanu@users.noreply.github.com>
2019-02-21 14:43:20 +0100

    [update_server] - added update server method (#1)
    * [update_server] - added update server method
    
    * minor refactor in update server method
    
    * [update_server] - refactor update server method; raise proper errors on invalid types;

--- pyserverpilot/modules/servers.py
+++ pyserverpilot/modules/servers.py
@@ -5,15 +5,31 @@ from pyserverpilot.models.server import Server
 
 ServerList = List[Server]
 
+SERVERS_BASE_ENDPOINT = 'servers'
+
 
 class Servers(Serverpilot):
     def get_servers(self) -> ServerList:
         servers = []
 
-        for server in self._request('GET', 'servers'):
+        for server in self._request('GET', SERVERS_BASE_ENDPOINT):
             servers.append(Server(server))
 
         return servers
 
     def get_server(self, id: str) -> Server:
-        return Server(self._request('GET', 'servers/{}'.format(id)))
+        return Server(self._request('GET', '{}/{}'.format(SERVERS_BASE_ENDPOINT, id)))
+
+    def delete_server(self, id: str) -> None:
+        self._request('DELETE', '{}/{}'.format(SERVERS_BASE_ENDPOINT, id))
+
+    def update_server(self, id: str, **params) -> Server:
+        for key, val in params.items():
+            if key == 'plan' and isinstance(val, str) is False:
+                raise TypeError('plan argument has to be of type string')
+            elif key in ['firewall', 'autoupdates', 'deny_unknown_domains'] and isinstance(val, bool) is False:
+                raise TypeError('{} argument has to be of type boolean'.format(key))
+            else:
+                raise AttributeError('Found unexpected argument: {}'.format(key))
+
+        return Server(self._request('POST', '{}/{}'.format(SERVERS_BASE_ENDPOINT, id), params))
