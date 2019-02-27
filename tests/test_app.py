a7d2fa572eb40ab47ec58736dae31061431543d7
Alex <alexraileanu@me.com>
2019-02-27 17:18:32 +0100

    [tests] - patch entire test class rather than each case individually; added get_app test

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,10 +13,10 @@ def client():
     yield Serverpilot.client('apps', client_id='test_id', api_key='test_key')
 
 
+@patch('pyserverpilot.serverpilot.requests.request')
 class TestApp(object):
     app: App  # to keep track throughout this test suite
 
-    @patch('pyserverpilot.serverpilot.requests.request')
     def test_create_app(self, mock_sp, client: AppsModule):
         mock_sp.return_value = MockSP('get_app')
         app_data = MockSP.create_app()['data']
@@ -24,3 +24,9 @@ class TestApp(object):
         response = client.create_app(**app_data)
         assert response.name == app_data['name']
         self.app = response
+
+    def test_get_app(self, mock_sp, client: AppsModule):
+        mock_sp.return_value = MockSP('get_app')
+
+        response = client.get_app(self.app.id)
+        assert response.id == self.app.id
