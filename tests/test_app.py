4a088fb1425a61abc9e2347d6b07f6bfbeab1fc6
Alex <alexraileanu@me.com>
2019-02-27 14:13:23 +0100

    [tests] - fixed mock issues; renamed db module to dbs

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,15 @@
+from unittest.mock import patch
+
+from pyserverpilot import Serverpilot
+from .mock_service import MockSP
+
+
 class TestApp(object):
-    def test_hello(self):
-        print('world')
+
+    @patch('pyserverpilot.serverpilot.requests.request')
+    def test_get_apps(self, mock_sp):
+        mock_sp.return_value = MockSP('get_apps')
+
+        cl = Serverpilot.client('apps', client_id='test_id', api_key='test_key')
+        r = cl.get_apps()
+        assert r[0].id == 'c77JD4gZooGjrF8K'
