9056fec17d0a00c69d3d8384f569005840a961e0
Alex <alexraileanu@me.com>
2019-03-04 14:34:24 +0100

    [db-tests] - minor structure change in the tests folder

--- pyserverpilot/modules/__init__.py
+++ pyserverpilot/modules/__init__.py
@@ -1,2 +1,4 @@
 from .apps import Apps
+from .dbs import Dbs
 from .servers import Servers
+from .sysusers import Sysusers
--- tests/mocks/app_mock.py
+++ tests/mocks/app_mock.py
@@ -1,17 +1,7 @@
-class MockSP:
-    status_code: int
-    to_call: str
+from tests.mocks.base import BaseMock
 
-    def __init__(self, method: str = '', status_code: int = 200):
-        self.to_call = method
-        self.status_code = status_code
 
-    def json(self):
-        if self.to_call != '':
-            return getattr(self, self.to_call)()
-        return {
-            'data': {}
-        }
+class AppMock(BaseMock):
 
     @classmethod
     def create_app(cls):
